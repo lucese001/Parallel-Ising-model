4a9ca566196461846a1d060ad7c8b62a68ff2d97
Luca Cerioni <luca.cerioniserret@gmail.com>
2026-01-19 15:41:55 +0100

    be

--- include/ising.hpp
+++ include/ising.hpp
@@ -93,7 +93,10 @@ inline int computeEnSiteDebug(const vector<int8_t>& conf,
             printf("  ");
             for (size_t x = 0; x < local_L_halo[1]; ++x) {
                 size_t idx_halo = coord_halo[0] + coord_halo[1] * local_L_halo[0];
+                cout << "idx_halo" << idx_halo << endl;
+                cout << "conf[idx_halo]" << static_cast<int>(conf[idx_halo]) << endl;
                 printf("%c ", conf[idx_halo] > 0 ? '+' : '-');
+                cout << endl;
             }
             printf("\n");
         }
